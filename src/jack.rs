bfaef73c7dbafdfb18f69f763f04aa9e576e88e5
Alex Norman <alex@x37v.info>
2020-11-29 18:09:22 -0800

    process midi out

--- src/jack.rs
+++ src/jack.rs
@@ -1,29 +1,63 @@
-use crate::sched::Sched;
+use crate::sched::{MidiValueQueue, Sched};
+use sched::pqueue::TickPriorityDequeue;
 pub struct Jack {
     client: Option<jack::AsyncClient<Notifications, SchedProcessHandler>>,
 }
 
 struct SchedProcessHandler {
     sched: Sched,
+    midi_out: jack::Port<jack::MidiOut>,
+    midi_queue: MidiValueQueue,
 }
 
 impl jack::ProcessHandler for SchedProcessHandler {
     fn process(&mut self, client: &jack::Client, ps: &jack::ProcessScope) -> jack::Control {
+        let now = self.sched.tick_next();
         self.sched
             .run(ps.n_frames() as usize, client.sample_rate() as usize);
+        {
+            let mut midi_out = self.midi_out.writer(ps);
+            let mut write_midi = |tick: u32, bytes: &[u8]| {
+                let _ = midi_out.write(&jack::RawMidi { time: tick, bytes });
+            };
+            let mut midi_queue = self.midi_queue.lock();
+            let next = self.sched.tick_next();
+            while let Some((t, midi)) = midi_queue.dequeue_lt(next) {
+                let tick = (if t < now { now } else { t } - now) as u32;
+                let iter = &mut midi.iter();
+                match iter.len() {
+                    3 => write_midi(
+                        tick,
+                        &[
+                            iter.next().unwrap(),
+                            iter.next().unwrap(),
+                            iter.next().unwrap(),
+                        ],
+                    ),
+                    2 => write_midi(tick, &[iter.next().unwrap(), iter.next().unwrap()]),
+                    1 => write_midi(tick, &[iter.next().unwrap()]),
+                    _ => (),
+                };
+            }
+        }
         jack::Control::Continue
     }
 }
 
 impl Jack {
-    pub fn new(mut sched: Sched) -> Self {
+    pub fn new(sched: Sched) -> Self {
         let (client, _status) =
             jack::Client::new("xsched", jack::ClientOptions::NO_START_SERVER).unwrap();
-        let mut midi_out = client
+        let midi_out = client
             .register_port("midi", jack::MidiOut::default())
             .expect("failed to create midi out port");
         let notify = Notifications::new();
-        let handler = SchedProcessHandler { sched };
+        let midi_queue = sched.midi_queue();
+        let handler = SchedProcessHandler {
+            sched,
+            midi_out,
+            midi_queue,
+        };
 
         // Activate the client, which starts the processing.
         let client = client
