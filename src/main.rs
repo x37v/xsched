ec50c678c62839445fa0f414e7fb60c03bdcb0fd
Alex Norman <alex@x37v.info>
2020-12-06 13:07:11 -0800

    gracefully exit

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,23 @@ mod sched;
 use crate::jack::Jack;
 use crate::sched::Sched;
 
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
 fn main() {
+    let run = Arc::new(AtomicBool::new(true));
+    //gracefully handle control-c
+    {
+        let run = run.clone();
+        ctrlc::set_handler(move || {
+            run.store(false, Ordering::Release);
+        })
+        .expect("Error setting Ctrl-C handler");
+    }
+
     let sched = Sched::new();
     let jack = Jack::new(sched);
+    while run.load(Ordering::Acquire) {
+        std::thread::sleep(std::time::Duration::from_millis(10));
+    }
 }
