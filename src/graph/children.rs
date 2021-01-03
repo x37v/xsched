61a134dab1505eae7c9c89da5b6a63eef7cc987e
Alex Norman <alex@x37v.info>
2021-01-03 15:09:03 -0800

    remove extra mutex

--- src/graph/children.rs
+++ src/graph/children.rs
@@ -22,7 +22,7 @@ pub enum Children {
 /// A wrapper for children that has swappable members.
 #[derive(Default)]
 pub struct SwapChildren {
-    children: Mutex<Arc<Mutex<Children>>>,
+    children: Mutex<Arc<Children>>,
     index_binding: Arc<BindingSwapSet<usize>>,
 }
 
@@ -30,7 +30,7 @@ pub struct SwapChildren {
 pub struct SwapChildrenContainer(Mutex<Arc<SwapChildren>>);
 
 impl SwapChildren {
-    pub fn swap(&self, children: Arc<Mutex<Children>>) -> Arc<Mutex<Children>> {
+    pub fn swap(&self, children: Arc<Children>) -> Arc<Children> {
         let mut g = self.children.lock();
         std::mem::replace(g.deref_mut(), children)
     }
@@ -42,7 +42,7 @@ impl SwapChildren {
 
 impl GraphChildExec for SwapChildren {
     fn child_count(&self) -> ChildCount {
-        match *self.children.lock().lock() {
+        match self.children.lock().as_ref() {
             Children::None => ChildCount::None,
             Children::NChild { .. } => ChildCount::Inf,
             Children::Indexed { ref children, .. } => ChildCount::Some(children.len()),
@@ -50,7 +50,7 @@ impl GraphChildExec for SwapChildren {
     }
 
     fn child_exec_range(&self, context: &mut dyn EventEvalContext, range: core::ops::Range<usize>) {
-        match self.children.lock().lock().deref_mut() {
+        match self.children.lock().as_ref() {
             Children::None => (),
             Children::NChild { child } => {
                 for i in range {
@@ -59,9 +59,9 @@ impl GraphChildExec for SwapChildren {
                 }
             }
             Children::Indexed { children } => {
-                let (_, r) = children.split_at_mut(range.start);
-                let (r, _) = r.split_at_mut(range.end - range.start);
-                for (i, c) in r.iter_mut().enumerate() {
+                let (_, r) = children.split_at(range.start);
+                let (r, _) = r.split_at(range.end - range.start);
+                for (i, c) in r.iter().enumerate() {
                     self.index_binding.set(i + range.start);
                     c.node_exec(context);
                 }
