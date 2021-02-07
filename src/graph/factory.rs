85b138b4a63cbf24e12bc4d3a8a89227fd937f0a
Alex Norman <alex@x37v.info>
2021-02-06 18:57:05 -0800

    prepare for more cases in graph factory

--- src/graph/factory.rs
+++ src/graph/factory.rs
@@ -23,59 +23,61 @@ pub fn create_instance(
     _args: Option<JsonValue>,
     queue_sources: &Arc<dyn QueueSource>,
 ) -> Result<GraphItem, CreateError> {
-    if type_name == "root::clock" {
-        let clock = ClockData::default();
-        let micros: Arc<BindingSwapGet<Float>> =
-            Arc::new(BindingSwapGet::new(clock.period_micros()));
-        let mut map = HashMap::new();
-        map.insert(
-            "period_micros",
-            ParamAccess::new_get(ParamGet::Float(micros.clone())),
-        );
-        Ok(GraphItem::new_root(
-            &"root::clock",
-            RootClock::new(micros.clone() as Arc<dyn ParamBindingGet<Float>>),
-            map,
-            Some(uuid),
-        ))
-    } else if type_name == "leaf::midi::note" {
-        let chan: Arc<BindingSwapGet<u8>> = Arc::new(BindingSwapGet::new(0));
-        let num: Arc<BindingSwapGet<u8>> = Arc::new(BindingSwapGet::new(64));
-        let on_vel: Arc<BindingSwapGet<u8>> = Arc::new(BindingSwapGet::new(127));
-        let off_vel: Arc<BindingSwapGet<u8>> = Arc::new(BindingSwapGet::new(127));
-        let dur: Arc<BindingSwapGet<TickResched>> =
-            Arc::new(BindingSwapGet::new(TickResched::ContextRelative(1)));
+    match type_name {
+        "root::clock" => {
+            let clock = ClockData::default();
+            let micros: Arc<BindingSwapGet<Float>> =
+                Arc::new(BindingSwapGet::new(clock.period_micros()));
+            let mut map = HashMap::new();
+            map.insert(
+                "period_micros",
+                ParamAccess::new_get(ParamGet::Float(micros.clone())),
+            );
+            Ok(GraphItem::new_root(
+                &"root::clock",
+                RootClock::new(micros.clone() as Arc<dyn ParamBindingGet<Float>>),
+                map,
+                Some(uuid),
+            ))
+        }
+        "leaf::midi::note" => {
+            let chan: Arc<BindingSwapGet<u8>> = Arc::new(BindingSwapGet::new(0));
+            let num: Arc<BindingSwapGet<u8>> = Arc::new(BindingSwapGet::new(64));
+            let on_vel: Arc<BindingSwapGet<u8>> = Arc::new(BindingSwapGet::new(127));
+            let off_vel: Arc<BindingSwapGet<u8>> = Arc::new(BindingSwapGet::new(127));
+            let dur: Arc<BindingSwapGet<TickResched>> =
+                Arc::new(BindingSwapGet::new(TickResched::ContextRelative(1)));
 
-        //setup parameters
-        let mut map = HashMap::new();
-        map.insert("chan", ParamAccess::new_get(ParamGet::U8(chan.clone())));
-        map.insert("num", ParamAccess::new_get(ParamGet::U8(num.clone())));
-        map.insert("on_vel", ParamAccess::new_get(ParamGet::U8(on_vel.clone())));
-        map.insert(
-            "off_vel",
-            ParamAccess::new_get(ParamGet::U8(off_vel.clone())),
-        );
-        map.insert(
-            "dur",
-            ParamAccess::new_get(ParamGet::TickResched(dur.clone())),
-        );
+            //setup parameters
+            let mut map = HashMap::new();
+            map.insert("chan", ParamAccess::new_get(ParamGet::U8(chan.clone())));
+            map.insert("num", ParamAccess::new_get(ParamGet::U8(num.clone())));
+            map.insert("on_vel", ParamAccess::new_get(ParamGet::U8(on_vel.clone())));
+            map.insert(
+                "off_vel",
+                ParamAccess::new_get(ParamGet::U8(off_vel.clone())),
+            );
+            map.insert(
+                "dur",
+                ParamAccess::new_get(ParamGet::TickResched(dur.clone())),
+            );
 
-        let note = ::sched::graph::midi::MidiNote::new(
-            chan as Arc<dyn ParamBindingGet<u8>>,
-            num as Arc<dyn ParamBindingGet<u8>>,
-            dur as Arc<dyn ParamBindingGet<TickResched>>,
-            on_vel as Arc<dyn ParamBindingGet<u8>>,
-            off_vel as Arc<dyn ParamBindingGet<u8>>,
-            queue_sources.midi_event_source(),
-            queue_sources.midi_queue() as _,
-        );
-        Ok(GraphItem::new_leaf(
-            &"leaf::midi::note",
-            note,
-            map,
-            Some(uuid),
-        ))
-    } else {
-        Err(CreateError::TypeNotFound)
+            let note = ::sched::graph::midi::MidiNote::new(
+                chan as Arc<dyn ParamBindingGet<u8>>,
+                num as Arc<dyn ParamBindingGet<u8>>,
+                dur as Arc<dyn ParamBindingGet<TickResched>>,
+                on_vel as Arc<dyn ParamBindingGet<u8>>,
+                off_vel as Arc<dyn ParamBindingGet<u8>>,
+                queue_sources.midi_event_source(),
+                queue_sources.midi_queue() as _,
+            );
+            Ok(GraphItem::new_leaf(
+                &"leaf::midi::note",
+                note,
+                map,
+                Some(uuid),
+            ))
+        }
+        _ => Err(CreateError::TypeNotFound),
     }
 }
