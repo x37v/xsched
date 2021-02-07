6da6aff752263275c33e57c6c2477de6bf47d309
Alex Norman <alex@x37v.info>
2021-02-06 19:26:49 -0800

    more graph items

--- src/graph/factory.rs
+++ src/graph/factory.rs
@@ -10,7 +10,7 @@ use sched::{
         swap::BindingSwapGet,
         ParamBindingGet,
     },
-    graph::root_clock::RootClock,
+    graph as sgraph,
     tick::TickResched,
     Float,
 };
@@ -23,6 +23,7 @@ pub fn create_instance(
     _args: Option<JsonValue>,
     queue_sources: &Arc<dyn QueueSource>,
 ) -> Result<GraphItem, CreateError> {
+    //TODO build.rs BindStoreNode for all the binding types
     match type_name {
         "root::clock" => {
             let clock = ClockData::default();
@@ -35,7 +36,75 @@ pub fn create_instance(
             );
             Ok(GraphItem::new_root(
                 &"root::clock",
-                RootClock::new(micros.clone() as Arc<dyn ParamBindingGet<Float>>),
+                sgraph::root_clock::RootClock::new(
+                    micros.clone() as Arc<dyn ParamBindingGet<Float>>
+                ),
+                map,
+                Some(uuid),
+            ))
+        }
+        "node::clock_ratio" => {
+            let mul: Arc<BindingSwapGet<usize>> = Arc::new(BindingSwapGet::new(1));
+            let div: Arc<BindingSwapGet<usize>> = Arc::new(BindingSwapGet::new(1));
+            let mut map = HashMap::new();
+            map.insert("mul", ParamAccess::new_get(ParamGet::USize(mul.clone())));
+            map.insert("div", ParamAccess::new_get(ParamGet::USize(div.clone())));
+
+            let node = sgraph::clock_ratio::ClockRatio::new(
+                mul as Arc<dyn ParamBindingGet<usize>>,
+                div as Arc<dyn ParamBindingGet<usize>>,
+            );
+            Ok(GraphItem::new_node(
+                &"node::clock_ratio",
+                node,
+                map,
+                Some(uuid),
+            ))
+        }
+        "node::gate" => {
+            let gate: Arc<BindingSwapGet<bool>> = Arc::new(BindingSwapGet::new(false));
+            let mut map = HashMap::new();
+            map.insert("gate", ParamAccess::new_get(ParamGet::Bool(gate.clone())));
+
+            let node = sgraph::gate::Gate::new(gate as Arc<dyn ParamBindingGet<bool>>);
+            Ok(GraphItem::new_node(&"node::gate", node, map, Some(uuid)))
+        }
+        "node::one_hot" => {
+            let sel: Arc<BindingSwapGet<usize>> = Arc::new(BindingSwapGet::new(0));
+            let mut map = HashMap::new();
+            map.insert("sel", ParamAccess::new_get(ParamGet::USize(sel.clone())));
+            let node = sgraph::one_hot::OneHot::new(sel as Arc<dyn ParamBindingGet<usize>>);
+            Ok(GraphItem::new_node(&"node::one_hot", node, map, Some(uuid)))
+        }
+        "node::fanout" => {
+            let node = sgraph::fanout::FanOut::new();
+            Ok(GraphItem::new_node(
+                &"node::fanout",
+                node,
+                HashMap::new(),
+                Some(uuid),
+            ))
+        }
+        "node::step_seq" => {
+            let step_ticks: Arc<BindingSwapGet<usize>> = Arc::new(BindingSwapGet::new(16));
+            let steps: Arc<BindingSwapGet<usize>> = Arc::new(BindingSwapGet::new(16));
+            let mut map = HashMap::new();
+            map.insert(
+                "step_ticks",
+                ParamAccess::new_get(ParamGet::USize(step_ticks.clone())),
+            );
+            map.insert(
+                "steps",
+                ParamAccess::new_get(ParamGet::USize(steps.clone())),
+            );
+
+            let node = sgraph::step_seq::StepSeq::new(
+                step_ticks as Arc<dyn ParamBindingGet<usize>>,
+                steps as Arc<dyn ParamBindingGet<usize>>,
+            );
+            Ok(GraphItem::new_node(
+                &"node::step_seq",
+                node,
                 map,
                 Some(uuid),
             ))
@@ -62,7 +131,7 @@ pub fn create_instance(
                 ParamAccess::new_get(ParamGet::TickResched(dur.clone())),
             );
 
-            let note = ::sched::graph::midi::MidiNote::new(
+            let note = sgraph::midi::MidiNote::new(
                 chan as Arc<dyn ParamBindingGet<u8>>,
                 num as Arc<dyn ParamBindingGet<u8>>,
                 dur as Arc<dyn ParamBindingGet<TickResched>>,
